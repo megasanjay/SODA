c44f1293c6ffafe5459a595a364b1e351f8f2d45
Sanjay Soundarajan <sanjays344@mail.fresnostate.edu>
2020-11-24 23:04:20 -0800

    updated app

--- src/main.js
+++ src/main.js
@@ -1,84 +1,88 @@
-const {app, BrowserWindow, dialog} = require('electron')
-app.showExitPrompt = true
-const path = require('path')
-const glob = require('glob')
-const contextMenu = require('electron-context-menu');
-const log  = require("electron-log");
-require('v8-compile-cache')
-const {ipcMain} = require('electron')
+const { app, BrowserWindow, dialog } = require("electron");
+app.showExitPrompt = true;
+const path = require("path");
+const glob = require("glob");
+const contextMenu = require("electron-context-menu");
+const log = require("electron-log");
+require("v8-compile-cache");
+const { ipcMain } = require("electron");
+const { autoUpdater } = require("electron-updater");
 
-log.transports.console.level = false
+log.transports.console.level = false;
 /*************************************************************
  * Python Process
  *************************************************************/
 
-const PY_DIST_FOLDER = 'pysodadist'
-const PY_FOLDER = 'pysoda'
-const PY_MODULE = 'api' // without .py suffix
+const PY_DIST_FOLDER = "pysodadist";
+const PY_FOLDER = "pysoda";
+const PY_MODULE = "api"; // without .py suffix
 
-let pyProc = null
-let pyPort = null
+let pyProc = null;
+let pyPort = null;
 
 const guessPackaged = () => {
-  const fullPath = path.join(__dirname, PY_DIST_FOLDER)
-  return require('fs').existsSync(fullPath)
-}
+  const fullPath = path.join(__dirname, PY_DIST_FOLDER);
+  return require("fs").existsSync(fullPath);
+};
 
 const getScriptPath = () => {
   if (!guessPackaged()) {
-    return path.join(__dirname, PY_FOLDER, PY_MODULE + '.py')
+    return path.join(__dirname, PY_FOLDER, PY_MODULE + ".py");
   }
-  if (process.platform === 'win32') {
-    return path.join(__dirname, PY_DIST_FOLDER, PY_MODULE, PY_MODULE + '.exe')
+  if (process.platform === "win32") {
+    return path.join(__dirname, PY_DIST_FOLDER, PY_MODULE, PY_MODULE + ".exe");
   }
-  return path.join(__dirname, PY_DIST_FOLDER, PY_MODULE, PY_MODULE)
-}
+  return path.join(__dirname, PY_DIST_FOLDER, PY_MODULE, PY_MODULE);
+};
 
 const selectPort = () => {
-  pyPort = 4242
-  return pyPort
-}
+  pyPort = 4242;
+  return pyPort;
+};
 
 const createPyProc = () => {
-  let script = getScriptPath()
-  let port = '' + selectPort()
+  let script = getScriptPath();
+  let port = "" + selectPort();
 
   if (guessPackaged()) {
-    pyProc = require('child_process').execFile(script, [port], { stdio: 'ignore' })
+    pyProc = require("child_process").execFile(script, [port], {
+      stdio: "ignore",
+    });
   } else {
-    pyProc = require('child_process').spawn('python', [script, port], { stdio: 'ignore' })
+    pyProc = require("child_process").spawn("python", [script, port], {
+      stdio: "ignore",
+    });
   }
 
   if (pyProc != null) {
-    console.log('child process success on port ' + port)
-    log.info('child process success on port ' + port)
+    console.log("child process success on port " + port);
+    log.info("child process success on port " + port);
   } else {
-    console.error('child process failed to start on port' + port)
+    console.error("child process failed to start on port" + port);
   }
-}
+};
 
 const exitPyProc = () => {
-  pyProc.kill()
-  pyProc = null
-  pyPort = null
-}
+  pyProc.kill();
+  pyProc = null;
+  pyPort = null;
+};
 
-app.on('ready', createPyProc)
-app.on('will-quit', exitPyProc)
-
+app.on("ready", createPyProc);
+app.on("will-quit", exitPyProc);
 
 /*************************************************************
  * Main app window
  *************************************************************/
 
-let mainWindow = null
+let mainWindow = null;
 
-function initialize () {
-  makeSingleInstance()
+function initialize() {
+  makeSingleInstance();
 
-  loadDemos()
+  loadDemos();
 
-  function createWindow () {
+  function createWindow() {
     const windowOptions = {
       minWidth: 1080,
       minHeight: 680,
@@ -86,56 +90,67 @@ function initialize () {
       height: 720,
       center: true,
       //title: app.getName(),
-      icon: __dirname + '/assets/menu-icon/soda_icon.png',
+      icon: __dirname + "/assets/menu-icon/soda_icon.png",
       webPreferences: {
-        nodeIntegration: true
-      }
-    }
+        nodeIntegration: true,
+      },
+    };
 
     //if (process.platform === 'linux') {
     //  windowOptions.icon = path.join(__dirname, '/assets/app-icon/png/soda_icon.png')
     //}
 
-    mainWindow = new BrowserWindow(windowOptions)
-    mainWindow.loadURL(path.join('file://', __dirname, '/index.html'))
+    mainWindow = new BrowserWindow(windowOptions);
+    mainWindow.loadURL(path.join("file://", __dirname, "/index.html"));
 
-/*    mainWindow.on('closed', () => {
+    mainWindow.webContents.openDevTools();
+
+    mainWindow.webContents.once('dom-ready', () => {
+      autoUpdater.checkForUpdatesAndNotify();
+    });
+
+    /*    mainWindow.on('closed', () => {
       mainWindow = null
     })*/
 
-    mainWindow.on('close', (e) => {
-    if (app.showExitPrompt) {
-        e.preventDefault() // Prevents the window from closing
-        dialog.showMessageBox(BrowserWindow.getFocusedWindow(), {
-            type: 'question',
-            buttons: ['Yes', 'No'],
-            title: 'Confirm',
-            message: 'Any running proccess will be stopped. Are you sure you want to quit?'
-        }, function (response) {
-            if (response === 0) { // Runs the following if 'Yes' is clicked
-                app.showExitPrompt = false
-                mainWindow.close()
-                /// feedback form iframe prevents closing gracefully
-                /// so force close
-                if (!mainWindow.closed) {
-                  mainWindow.destroy()
-                }
+    mainWindow.on("close", (e) => {
+      if (app.showExitPrompt) {
+        e.preventDefault(); // Prevents the window from closing
+        dialog.showMessageBox(
+          BrowserWindow.getFocusedWindow(),
+          {
+            type: "question",
+            buttons: ["Yes", "No"],
+            title: "Confirm",
+            message:
+              "Any running proccess will be stopped. Are you sure you want to quit?",
+          },
+          function (response) {
+            if (response === 0) {
+              // Runs the following if 'Yes' is clicked
+              app.showExitPrompt = false;
+              mainWindow.close();
+              /// feedback form iframe prevents closing gracefully
+              /// so force close
+              if (!mainWindow.closed) {
+                mainWindow.destroy();
+              }
             }
-        })
-    }
-    })
-
+          }
+        );
+      }
+    });
   }
 
-  app.on('ready', () => {
-    createWindow()
-  })
+  app.on("ready", () => {
+    createWindow();
+  });
 
-  app.on('window-all-closed', () => {
+  app.on("window-all-closed", () => {
     // if (process.platform !== 'darwin') {
-      app.quit()
+    app.quit();
     // }
-  })
+  });
 
   // app.on('activate', () => {
   //   if (mainWindow === null) {
@@ -145,21 +160,20 @@ function initialize () {
 }
 
 // Make this app a single instance app.
-const gotTheLock = app.requestSingleInstanceLock()
+const gotTheLock = app.requestSingleInstanceLock();
 
-function makeSingleInstance () {
-  if (process.mas) return
+function makeSingleInstance() {
+  if (process.mas) return;
 
   if (!gotTheLock) {
-    app.quit()
+    app.quit();
   } else {
-
-    app.on('second-instance', () => {
+    app.on("second-instance", () => {
       if (mainWindow) {
-        if (mainWindow.isMinimized()) mainWindow.restore()
-        mainWindow.focus()
+        if (mainWindow.isMinimized()) mainWindow.restore();
+        mainWindow.focus();
       }
-    })
+    });
   }
 }
 
@@ -169,26 +183,43 @@ If you check your download folder, you'll see it there.
 See: https://github.com/nteract/nteract/issues/1655
 showSaveImageAs prompts the users where they want to save the image.
 */
-contextMenu()
+contextMenu();
 
 // Require each JS file in the main-process dir
-function loadDemos () {
-  const files = glob.sync(path.join(__dirname, 'main-process/**/*.js'))
-  files.forEach((file) => { require(file) })
+function loadDemos() {
+  const files = glob.sync(path.join(__dirname, "main-process/**/*.js"));
+  files.forEach((file) => {
+    require(file);
+  });
 }
 
-initialize()
+initialize();
 
-
-ipcMain.on('resize-window', (event, dir) => {
-  var x = mainWindow.getSize()[0]
-  var y = mainWindow.getSize()[1]
-  if (dir === 'up'){
-    x = x+1
-    y = y+1
+ipcMain.on("resize-window", (event, dir) => {
+  var x = mainWindow.getSize()[0];
+  var y = mainWindow.getSize()[1];
+  if (dir === "up") {
+    x = x + 1;
+    y = y + 1;
   } else {
-    x = x-1
-    y = y-1
+    x = x - 1;
+    y = y - 1;
   }
-  mainWindow.setSize(x, y)
-})
+  mainWindow.setSize(x, y);
+});
+
+ipcMain.on("app_version", (event) => {
+  event.sender.send("app_version", { version: app.getVersion() });
+});
+
+autoUpdater.on("update-available", () => {
+  mainWindow.webContents.send("update_available");
+});
+
+autoUpdater.on("update-downloaded", () => {
+  mainWindow.webContents.send("update_downloaded");
+});
+
+ipcMain.on("restart_app", () => {
+  autoUpdater.quitAndInstall();
+});
